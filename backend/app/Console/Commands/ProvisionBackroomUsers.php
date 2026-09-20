151e9deb49cd3422de3fde1b365cbb7aa0b67bd4
Mark <fernandezromark_bspsy@plmun.edu.ph>
2026-09-20 18:24:48 +0800

    fix: format ProvisionBackroomUsers command

--- backend/app/Console/Commands/ProvisionBackroomUsers.php
+++ backend/app/Console/Commands/ProvisionBackroomUsers.php
@@ -2,8 +2,6 @@
 
 namespace App\Console\Commands;
 
-use App\Integrations\SupabaseAdminClient;
-use App\Integrations\SupabaseAdminException;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
